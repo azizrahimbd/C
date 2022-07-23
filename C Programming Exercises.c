47285a0dad1406b30988467dde5b6b285f3649ab
Aziz Rahim <azizrahim2005@gmail.com>
2022-07-23 18:39:39 +0600

    Write a C program to print your name, date of birth. and mobile number.

--- C Programming Exercises.c
+++ C Programming Exercises.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+// Write a C program to print your name, date of birth. and mobile number.
 
+int main()
+{
+    printf("Name: Aziz Rahim\n");
+    printf("DOB: October 19,2005\n");
+    printf("Mobile: +8801717330616\n");
+
+    return 0;
+}
